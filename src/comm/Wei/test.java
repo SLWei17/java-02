d36308e4349165fa190fa6108874abca3a4d6076
Wei <1549938590@qq.com>
2022-07-20 15:31:33 +0800

    Third commit

--- src/comm/Wei/test.java
+++ src/comm/Wei/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args){
         System.out.println("Hello IDEA!");
         System.out.println("Git V2");
+        System.out.println("Git V3");
     }
 
 }
