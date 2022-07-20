49529f929cd8a7d650ac6be6bf7b98a5279a535d
Wei <1549938590@qq.com>
2022-07-20 15:49:12 +0800

    master test

--- src/comm/Wei/test.java
+++ src/comm/Wei/test.java
@@ -6,6 +6,7 @@ public class test {
         System.out.println("Git V2");
         System.out.println("Git V3");
         System.out.println("hot-fix");
+        System.out.println("master commit");
     }
 
 }
